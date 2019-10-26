db2f09136cc82fe1b2ee1282c1b9ada3efb26c12
leoleonzi <leonardo.leonzi@hotmail.com>
2019-10-27 00:35:03 +0200

    implemented httpClient

---
+++ src/services/httpClient.ts
@@ -0,0 +1,30 @@
+import * as request from 'request';
+
+export class HttpClient {
+
+    public get = (url: string): Promise<any> => {
+        return new Promise((resolve, reject) => {
+            request
+                .get(url)
+                .on('response', function(response) {
+                    resolve(response);
+                })
+                .on('error', function(err) {
+                    reject(err);
+                })
+        });
+    }
+
+    public post = (url: string, body: object): Promise<any> => {
+        return new Promise((resolve, reject) => {
+            request
+                .post({url, formData: body})
+                .on('response', function(response) {
+                    resolve(response);
+                })
+                .on('error', function(err) {
+                    reject(err);
+                })
+        });
+    }
+}
